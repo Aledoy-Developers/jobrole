0a6dd4d5efc76f1e726dc3cfe22e4db596b8ee49
Lu Abikoye <luabikoye@yahoo.com>
2025-06-11 23:50:24 +0100

    Added home page to header

--- header.php
+++ header.php
@@ -1,19 +1,20 @@
 <header>
-        <div class="container">
-            <div class="header-content">
-                <a href="index.php" class="logo"><img src="images/logo.png"></span></a>
-                <button class="mobile-menu-btn" id="mobileMenuBtn">
-                    <i class="fas fa-bars"></i>
-                </button>
-                <nav>
-                    <ul id="navMenu">
-                        <li><a href="about.php">About</a></li>
-                        <li><a href="speaker.php">Speakers</a></li>
-                        <li><a href="agenda.php">Agenda</a></li>
-                        <!-- <li><a href="award.php">Awards</a></li> -->
-                        <li><a href="register.php">Register</a></li>
-                    </ul>
-                </nav>
-            </div>
+    <div class="container">
+        <div class="header-content">
+            <a href="index.php" class="logo"><img src="images/logo.png"></span></a>
+            <button class="mobile-menu-btn" id="mobileMenuBtn">
+                <i class="fas fa-bars"></i>
+            </button>
+            <nav>
+                <ul id="navMenu">
+                    <li><a href="index.php">Home</a></li>
+                    <li><a href="about.php">About</a></li>
+                    <li><a href="speaker.php">Speakers</a></li>
+                    <li><a href="agenda.php">Agenda</a></li>
+                    <!-- <li><a href="award.php">Awards</a></li> -->
+                    <li><a href="register.php">Register</a></li>
+                </ul>
+            </nav>
         </div>
-    </header>
+    </div>
+</header>
