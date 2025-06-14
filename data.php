45bfa5937928221c5cb89f5c4828aa5b94b97719
Lu Abikoye <luabikoye@yahoo.com>
2025-06-14 13:26:56 +0100

    Updated data retrieval

---
+++ data.php
@@ -0,0 +1,23 @@
+<?php
+
+include('conn.php');
+
+echo '<h2>Registered Users</h2>';
+
+$res = mysqli_query($db,"select * from registration order by id desc");
+$num = mysqli_num_rows($res);
+
+echo '<h3> Total Records: '.$num.'</h3>';
+for($i=0; $i<$num; $i++)
+{
+    $row = mysqli_fetch_array($res);
+    echo 'Date: '.date('D d M Y h:i:s',strtotime($row['date_created'])).'<br>';
+    echo 'Full name: '.$row['fullname'].'<br>';
+    echo 'Email: '.$row['email'].'<br>';
+    echo 'Phone: '.$row['phone'].'<br>';
+    echo 'Organization: '.$row['organization'].'<br>';
+    echo 'Position: '.$row['position'].'<br>';
+    echo 'Participant: '.$row['attend'].'<br>';
+    echo '<hr>';
+}
+?>
