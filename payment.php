f947bf2f4a2c8c53a68d3ef1fa3185db2169bc59
olasumbo mayowa delu <mayowadelu@gmail.com>
2026-01-30 16:11:58 +0100

    payment page text]

--- payment.php
+++ payment.php
@@ -1,3 +1,6 @@
+<?php
+include('conn.php');
+?>
 <html>
 
 <head>
@@ -5,7 +8,6 @@
     <link rel="shortcut icon" href="image/icon.png" />
     <script src="jquery.js"></script>
     <style>
-
         body {
             background-color: #ccc;
             font-family: "Segoe UI", Tahoma, Geneva, Verdana, sans-serif;
@@ -19,13 +21,8 @@
         <a href="index"><img src="./dist/images/logo.png" alt="" class="img-fluid"></a>
         <h2>Jobrole Secure Payment Page</h2>
 
-        You can transfer the sum of &#x20A6;20,000 to the bank details below:<br><br><strong> </strong>
-        Account Number: <strong>1023747141</strong> <br>
-
-        Bank: <strong>United Bank for Africa</strong><br>
-
-        Account Name: <strong>Jobrole Consulting Limited.</strong><br><br>
-        -- OR -- <br><br>
+        <p>You are one step away from completing your registration!
+            Click the button below to pay securely with Paystack.</p>
         <form id="paymentForm">
             <div>
                 <input type="hidden" id="email-address" required value="<?php echo $email; ?>" />
@@ -50,38 +47,36 @@
 <script src="https://js.paystack.co/v1/inline.js"></script>
 
 <script>
+    const paymentForm = document.getElementById("paymentForm");
+    paymentForm.addEventListener("submit", payWithPaystack, false);
+    const firstName = document.getElementById("name");
+    const phone = document.getElementById("phone");
+    const pitch = document.getElementById("pitch");
+    if (window.location.hostname === "localhost" || window.location.hostname === "127.0.0.1") {
+        let key = "pk_test_59e6e667d3990cda6c6a6849eb0c8c303cc333fd";
+    } else {
+        let key = "pk_live_09914a9ba5e76f04a0baaa5db1b4a45958af48f9";
+    }
 
- const paymentForm = document.getElementById("paymentForm");
-paymentForm.addEventListener("submit", payWithPaystack, false);
-const firstName = document.getElementById("name");
-const phone = document.getElementById("phone");
-const pitch = document.getElementById("pitch");
-if (window.location.hostname === "localhost" || window.location.hostname === "127.0.0.1") {
-	let key = "pk_test_59e6e667d3990cda6c6a6849eb0c8c303cc333fd";
-} else {
-	let key = "pk_live_09914a9ba5e76f04a0baaa5db1b4a45958af48f9";
-}
-function payWithPaystack(e) {
-	e.preventDefault();
-	let handler = PaystackPop.setup({
-		key: "pk_live_09914a9ba5e76f04a0baaa5db1b4a45958af48f9", // Replace with your public key
-		email: document.getElementById("email-address").value,
-		amount: document.getElementById("amount").value * 100,
-		ref: "" + Math.floor(Math.random() * 1000000000 + 1), // generates a pseudo-unique reference. Please replace with a reference you generated. Or remove the line entirely so our API will generate one for you
-		// label: "Optional string that replaces customer email"
-		onClose: function () {
-			alert("Window closed.");
-		},
-		callback: function (response) {
-			//   let message = 'Payment complete! Reference: ' + response.reference;
-			//   alert(message);
-			document.location.href = 'payment-complete.php?em=<?php echo base64_encode($email); ?>&name=<?php echo base64_encode($fullname); ?>&p=<?php echo base64_encode($phone); ?>&s=<?php echo base64_encode($pitch_session); ?>&reference='+response.reference;
-		},
-	});
-	handler.openIframe();
-}
-
-
+    function payWithPaystack(e) {
+        e.preventDefault();
+        let handler = PaystackPop.setup({
+            key: "pk_live_09914a9ba5e76f04a0baaa5db1b4a45958af48f9", // Replace with your public key
+            email: document.getElementById("email-address").value,
+            amount: document.getElementById("amount").value * 100,
+            ref: "" + Math.floor(Math.random() * 1000000000 + 1), // generates a pseudo-unique reference. Please replace with a reference you generated. Or remove the line entirely so our API will generate one for you
+            // label: "Optional string that replaces customer email"
+            onClose: function() {
+                alert("Window closed.");
+            },
+            callback: function(response) {
+                //   let message = 'Payment complete! Reference: ' + response.reference;
+                //   alert(message);
+                document.location.href = 'payment-complete.php?em=<?php echo base64_encode($email); ?>&name=<?php echo base64_encode($fullname); ?>&p=<?php echo base64_encode($phone); ?>&s=<?php echo base64_encode($pitch_session); ?>&reference=' + response.reference;
+            },
+        });
+        handler.openIframe();
+    }
 </script>
 
 </html>
