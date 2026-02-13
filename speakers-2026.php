f8bf73b39c858f351a1db2f0ba7b8ca161706e14
kayceeosad <kosadebe364@gmail.com>
2026-02-13 17:01:59 +0100

    new speakers added

--- speakers-2026.php
+++ speakers-2026.php
@@ -43,6 +43,91 @@
                 <!-- Speaker 4 -->
 
                 <div class="speaker-card">
+                    <img src="images/Babatunde Akin.jpg.jpeg" alt="Babatunde Akin" class="speaker-image">
+                    <div class="speaker-info">
+                        <h3>Babatunde Akin</h3>
+                        <p class="position">Co-Founder and CEO of Sycamore Group</p>
+                        <p class="bio">Babatunde Akin-Moses is the Co-Founder and CEO of Sycamore Group, a borderless fintech providing credit, savings, investment, and related financial services to individuals and growing
+businesses. The platform has served over 400,000 customers across multiple markets, with a focus on users underserved by traditional financial institutions.</p>
+                        <!-- <div class="social-links">
+                        <a href="#"><i class="fab fa-twitter"></i></a>
+                        <a href="#"><i class="fab fa-linkedin"></i></a>
+                    </div> -->
+                    </div>
+                </div>
+                <div class="speaker-card">
+                    <img src="images/Kelechi .jpg.jpeg" alt="Kelechi Enyioha" class="speaker-image">
+                    <div class="speaker-info">
+                        <h3>Kelechi Enyioha 
+                        </h3>
+                        <p class="position">Enterprise Digital Transformation Leader .
+                        </p>
+                        <p class="bio">Kelechi Enyioha is an Enterprise Digital Transformation Leader with over 18 years of experience designing and delivering large-scale digital, data, and AI-enabled transformation in complex and highly regulated environments..</p>
+                        <!-- <div class="social-links">
+                        <a href="#"><i class="fab fa-twitter"></i></a>
+                        <a href="#"><i class="fab fa-linkedin"></i></a>
+                    </div> -->
+                    </div>
+                </div>
+                <div class="speaker-card">
+                    <img src="images/chisom.jpg" alt="Chisom Emenalom" class="speaker-image">
+                    <div class="speaker-info">
+                        <h3>Chisom Emenalom BL CPP
+                        </h3>
+                        <p class="position">Seasoned expert in Legal and Enterprise risk management with over 15 years of experience.
+                        </p>
+                        <p class="bio">Throughout her career, Chisom has worked with various companies to restructure business operations, strategy and management while leveraging on her expertise to drive growth and efficiency.</p>
+                        <!-- <div class="social-links">
+                        <a href="#"><i class="fab fa-twitter"></i></a>
+                        <a href="#"><i class="fab fa-linkedin"></i></a>
+                    </div> -->
+                    </div>
+                </div>
+                <div class="speaker-card">
+                    <img src="images/Nigel Newman.png" alt="Nigel Newman" class="speaker-image">
+                    <div class="speaker-info">
+                        <h3>Nigel Newman
+                        </h3>
+                        <p class="position">Co-Founder & Managing Director, Big 5 Assessment Ltd
+                        </p>
+                        <p class="bio">Nigel Newman is a highly experienced facilitator and a Global Master Practitioner in Dr. Edward de Bono's Thinking Methods. Nigel is a former military fast jet pilot and is a teaching graduate with a background in business management where he became experienced in the use of psychometric assessment in the recruitment and development of effective personnel</p>
+                        <!-- <div class="social-links">
+                        <a href="#"><i class="fab fa-twitter"></i></a>
+                        <a href="#"><i class="fab fa-linkedin"></i></a>
+                    </div> -->
+                    </div>
+                </div>
+                <div class="speaker-card">
+                    <img src="images/Sandra Ihenacho.jpg.jpeg" alt="Sandra Ihenacho" class="speaker-image">
+                    <div class="speaker-info">
+                        <h3>CEO, M.Bryan Consulting Limited
+                        </h3>
+                        <p class="position">Seasoned expert in Legal and Enterprise risk management with over 15 years of experience.
+                        </p>
+                        <p class="bio">Sandra Ihenacho, known as The Oracle, is a globally respected authority on Training Transfer Effectiveness (TTE) and the CEO of M.Bryan Consulting Limited, a Human Capital Development firm dedicated to ensuring that training delivers measurable performance,Business Impact, ROE, and ROI.</p>
+                        <!-- <div class="social-links">
+                        <a href="#"><i class="fab fa-twitter"></i></a>
+                        <a href="#"><i class="fab fa-linkedin"></i></a>
+                    </div> -->
+                    </div>
+                </div>
+                <div class="speaker-card">
+                    <img src="images/Sola Oladunjoye Pic.jpg.jpeg" alt="Sola Oladunjoye" class="speaker-image">
+                    <div class="speaker-info">
+                        <h3>Sola Oladunjoye
+                        </h3>
+                        <p class="position">CEO of Aigle Limited,
+                        </p>
+                        <p class="bio">Sola Oladunjoye is the CEO of Aigle Limited, a financial services company advancing access to SME financing.
+With nearly 20 years of experience in banking and investment, including roles at Standard Chartered and Renaissance Capital, he brings deep expertise in finance and strategy. A Covenant University alumnus, he is also committed to mentor-ship, leadership, and social impact.</p>
+                        <!-- <div class="social-links">
+                        <a href="#"><i class="fab fa-twitter"></i></a>
+                        <a href="#"><i class="fab fa-linkedin"></i></a>
+                    </div> -->
+                    </div>
+                </div>
+
+                <div class="speaker-card">
                     <img src="images/paul.jpg" alt="Paul Inwuanibe" class="speaker-image">
                     <div class="speaker-info">
                         <h3>Mr Paul Onwuanibe</h3>
